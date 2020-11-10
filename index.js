e2004707b766177f91c6fa7ba5fc65410039831d
Jake Fireside <labountypaul@icloud.com>
2020-11-10 14:28:17 -0600

    update command line function

--- index.js
+++ index.js
@@ -1,16 +1,66 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-];
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// array of questions for user
+const questions = () =>
+    inquirer.prompt([
+        {
+          type: 'input',
+          name: 'title',
+          message: 'Title?',
+        },
+        {
+          type: 'input',
+          name: 'table of contents',
+          message: 'Table of Contents?',
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Description?',
+        },
+        {
+          type: 'input',
+          name: 'installation',
+          message: 'Installation Instructions?',
+        },
+        {
+          type: 'input',
+          name: 'usage',
+          message: 'Usage Information',
+        },
+        {
+          type: 'input',
+          name: 'contributing',
+          message: 'Contributors?',
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'Licenses?',
+            choices: ('')
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Enter your Github username?',
+        },
+      ])
+
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(answers) {
+     return `Title:  ${answers.title}`
 }
 
-// function to initialize program
-function init() {
 
-}
+
 
 // function call to initialize program
-init();
+questions()
+  .then((answers) => writeFileAsync('test.md', writeToFile(answers)))
+  .then(() => console.log('Successfully wrote to test.md'))
+  .catch((err) => console.error(err));
